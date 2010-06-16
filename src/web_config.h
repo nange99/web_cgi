c14f3c505bd2315d12024691edb9afd43f3c0530
Thomás Alimena Del Grande <tgrande@pd3.com.br>
2010-06-16 11:02:55 -0300

    - Libconfig includes.

--- src/web_config.h
+++ src/web_config.h
@@ -8,6 +8,54 @@
 #ifndef WEB_CONFIG_H_
 #define WEB_CONFIG_H_
 
+/* Libconfig includes */
+#include <libconfig/options.h>
+#include <libconfig/acl.h>
+#include <libconfig/args.h>
+#include <libconfig/cish_defines.h>
+#include <libconfig/config_fetcher.h>
+#include <libconfig/device.h>
+#include <libconfig/typedefs.h>
+#include <libconfig/ip.h>
+#include <libconfig/dev.h>
+#include <libconfig/dhcp.h>
+#include <libconfig/dns.h>
+#include <libconfig/lan.h>
+#include <libconfig/ppp.h>
+#include <libconfig/str.h>
+#include <libconfig/libtime.h>
+#include <libconfig/flashsave.h>
+#include <libconfig/mangle.h>
+#include <libconfig/nat.h>
+#include <libconfig/ntp.h>
+#include <libconfig/nv.h>
+#include <libconfig/pam.h>
+#include <libconfig/pim.h>
+#include <libconfig/defines.h>
+#include <libconfig/version.h>
+#include <libconfig/debug.h>
+#include <libconfig/qos.h>
+#include <libconfig/ipsec.h>
+#include <libconfig/exec.h>
+#include <libconfig/process.h>
+#include <libconfig/quagga.h>
+#include <libconfig/snmp.h>
+#include <libconfig/ppcio.h>
+#include <libconfig/md5.h>
+
+#ifdef OPTION_MODEM3G
+#include <libconfig/modem3G.h>
+#endif
+#ifdef OPTION_SMCROUTE
+#include <libconfig/smcroute.h>
+#endif
+#include <libconfig/tunnel.h>
+#ifdef OPTION_VRRP
+#include <libconfig/vrrp.h>
+#endif
+#include <libconfig/ssh.h>
+#include <libconfig/vlan.h>
+
 #define DEBUG
 #ifdef DEBUG
 #define web_dbg(x,...) \
